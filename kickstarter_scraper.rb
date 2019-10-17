802e24635e5fcf197bf2ab1a4d38f334383d15a2
Zachary Schulz <zjschulz@gmail.com>
2019-10-17 23:54:48 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,5 +2,6 @@ require '../fixtures/kickstarter.html'
 # require libraries/modules here
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
 end
