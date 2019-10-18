f7d49d16ab33b211dddc3e947651609e7db7e3f0
Zachary Schulz <zjschulz@gmail.com>
2019-10-18 00:06:00 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,12 +4,12 @@ require 'pry'
 
 def create_project_hash
   projects = {}
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
 
-  
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
  
-  
   projects
   
 end
