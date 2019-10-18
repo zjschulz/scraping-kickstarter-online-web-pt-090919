de30977097b1bdaf0f60774eb45c9481e0aa43e0
Zachary Schulz <zjschulz@gmail.com>
2019-10-18 00:03:09 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,3 +12,4 @@ create_project_hash
 
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
