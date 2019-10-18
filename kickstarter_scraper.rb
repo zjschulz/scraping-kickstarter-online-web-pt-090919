b396903157aab706e1efc258ac2c6829df8e47e4
Zachary Schulz <zjschulz@gmail.com>
2019-10-18 00:05:47 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,9 +8,7 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   
-  kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
-  end
+ 
   
   projects
   
